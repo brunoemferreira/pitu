9b09ecad62ddc0657227cd5d697029682726fa62
brunoemferreira <brunoemf@gmail.com>
2020-11-23 21:29:24 -0300

    Aula 01 - 47:23 min

---
+++ backend/Readme.md
@@ -0,0 +1,13 @@
+```
+|- /src/
+  |- /controllers/
+  |- /models/
+  |- /routes/
+
+- Pastas :
+src         : pasta raiz do projeto;
+controllers : pasta onde ficam as regras de negócio das rotas;
+models      : pasta onde ficam as entidades do projeto;
+routes      : pasta onde ficam as rotas do projeto;
+
+```
--- backend/src/routes/links.ts
+++ backend/src/routes/links.ts
@@ -9,8 +9,8 @@ const router = Router();
  */
 router.post('/links', linksController.postLink);
 
-router.get('/links/:code', linksController.getLink);
+router.get('/links/:code', linksController.hitLink);
 
-router.get('/links/:code/stats', linksController.hitLink);
+router.get('/links/:code/stats', linksController.getLink);
 
 export default router;
