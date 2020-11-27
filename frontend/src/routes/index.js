f5a03fa1d12f7246f53a175386f81a261e87a14c
brunoemferreira <brunoemf@gmail.com>
2020-11-26 23:30:17 -0300

    Aula 02 - Rotas

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Rotas
+//
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route />
+      </Switch>
+    </Router>
+  );
+}
