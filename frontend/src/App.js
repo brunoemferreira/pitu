88dfab72aae9f1d103dc44a0f971085eef25ad13
brunoemferreira <brunoemf@gmail.com>
2020-11-26 23:19:38 -0300

    Aula 02 - 22:00 min

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
@@ -7,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pitu <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
