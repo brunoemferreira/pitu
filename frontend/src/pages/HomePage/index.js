aadae33b951b5a6bc5d805c5d68823e0aaff49f1
brunoemferreira <brunoemf@gmail.com>
2020-11-28 00:11:09 -0300

    Aula 02 - 01:39:29min

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,20 +1,74 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import Header from "./../../components/Header/index";
+import { ContentContainer, Form } from "./styles";
+import ShortnerService from "./../../services/shortnerService";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Alert,
+} from "react-bootstrap";
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLoading: false,
+      url: "",
+      code: "",
+      errorMessage: "",
+    };
   }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { url } = this.state;
+
+    this.setState({ isLoading: true, errorMessage: "" });
+
+    if (!url) {
+      this.setState({
+        isLoading: false,
+        errorMessage: "Informe uma url para encurtar.",
+      });
+    } else {
+      try {
+        const service = new ShortnerService();
+        const result = await service.generate({ url });
+
+        this.setState({ isLoading: false, code: result.code });
+      } catch (error) {
+        this.setState({
+          isLoading: false,
+          errorMessage: "Ops, Ocorreu um erro ao tentar encurtar a URL.",
+        });
+      }
+    }
+  };
 
   render() {
     return (
       <Container>
         <Header>Seu novo encurtador de URL. :)</Header>
-        <FontAwesomeIcon icon="check-square" />
-        Pitu
+        <ContentContainer>
+          <Form onSubmit={this.handleSubmit}>
+            <InputGroup>
+              <FormControl
+                placeholder="Cole seu link aqui..."
+                defaultValue=""
+                onChange={(e) => this.setState({ url: e.target.value })}
+              />
+              <InputGroup.Append>
+                <Button variant="primary" type="submit">
+                  Encurtar
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </ContentContainer>
       </Container>
     );
   }
---
+++ frontend/src/pages/HomePage/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ContentContainer = styled.div`
+  display: flex;
+  justify-content: center !important;
+  margin-bottom: 2rem;
+`;
+
+export const Form = styled.form`
+  flex: 0 0 80%;
+  max-width: 80%;
+  padding: 2rem 2rem 1rem 2rem;
+  border: solid 1px #ccc;
+  border-radius: 0.25rem;
+  text-align: center;
+`;
